618b294b2e3f63b4277fbaff449223a7345f078e
Casper da Costa-Luis <casper.dcl@physics.org>
2019-11-15 12:32:15 +0000

    fix 100% bar display
    - fixes #16
    - closes #17

--- index.js
+++ index.js
@@ -60,16 +60,17 @@ async function updateGist(stats) {
 function generateBarChart(percent, size) {
   const syms = "░▏▎▍▌▋▊▉█";
 
-  const frac = (size * 8 * percent) / 100;
+  const frac = Math.floor((size * 8 * percent) / 100);
   const barsFull = Math.floor(frac / 8);
+  if (barsFull >= size) {
+    return syms.substring(8, 9).repeat(size);
+  }
   const semi = frac % 8;
-  const barsEmpty = size - barsFull - 1;
 
   return [
     syms.substring(8, 9).repeat(barsFull),
     syms.substring(semi, semi + 1),
-    syms.substring(0, 1).repeat(barsEmpty)
-  ].join("");
+  ].join("").padEnd(size, syms.substring(0, 1));
 }
 
 (async () => {
