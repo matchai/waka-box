9af78dc95ca6d0de03ad33f45f75972798a46db1
BSKY <git@bsky.moe>
2019-12-27 07:56:08 +0900

    Migrate from Docker action to JavaScript action (#23)

--- index.js
+++ index.js
@@ -67,10 +67,9 @@ function generateBarChart(percent, size) {
   }
   const semi = frac % 8;
 
-  return [
-    syms.substring(8, 9).repeat(barsFull),
-    syms.substring(semi, semi + 1),
-  ].join("").padEnd(size, syms.substring(0, 1));
+  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+    .join("")
+    .padEnd(size, syms.substring(0, 1));
 }
 
 (async () => {
