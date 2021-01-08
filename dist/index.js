4a50e40a85b15405bb06af34963dad62e86967ff
Matan Kushner <hello@matchai.dev>
2021-01-07 22:47:32 -0500

    chore: rebuild

--- dist/index.js
+++ dist/index.js
@@ -847,6 +847,9 @@ module.exports = (function(e, t) {
       const e = await p.getMyStats({ range: i.LAST_7_DAYS });
       await updateGist(e);
     }
+    function trimRightStr(e, t) {
+      return e.length > t ? e.substring(0, t - 3) + "..." : e;
+    }
     async function updateGist(e) {
       let t;
       try {
@@ -859,13 +862,14 @@ module.exports = (function(e, t) {
         const n = e.data.languages[t];
         const { name: i, percent: s, text: o } = n;
         const a = [
-          i.padEnd(11),
+          trimRightStr(i, 11).padEnd(11),
           o.padEnd(14),
           generateBarChart(s, 21),
           String(s.toFixed(1)).padStart(5) + "%"
         ];
         r.push(a.join(" "));
       }
+      if (r.length == 0) return;
       try {
         const e = Object.keys(t.data.files)[0];
         await c.gists.update({
@@ -1525,6 +1529,7 @@ module.exports = (function(e, t) {
   },
   215: function(e) {
     e.exports = {
+      _args: [["@octokit/rest@16.36.0", "/Users/matan/dev/waka-box"]],
       _from: "@octokit/rest@16.36.0",
       _id: "@octokit/rest@16.36.0",
       _inBundle: false,
@@ -1545,12 +1550,10 @@ module.exports = (function(e, t) {
       },
       _requiredBy: ["/"],
       _resolved: "https://registry.npmjs.org/@octokit/rest/-/rest-16.36.0.tgz",
-      _shasum: "99892c57ba632c2a7b21845584004387b56c2cb7",
-      _spec: "@octokit/rest@16.36.0",
-      _where: "/Users/soramorimoto/src/github.com/matchai/waka-box",
+      _spec: "16.36.0",
+      _where: "/Users/matan/dev/waka-box",
       author: { name: "Gregor Martynus", url: "https://github.com/gr2m" },
       bugs: { url: "https://github.com/octokit/rest.js/issues" },
-      bundleDependencies: false,
       bundlesize: [{ path: "./dist/octokit-rest.min.js.gz", maxSize: "33 kB" }],
       contributors: [
         { name: "Mike de Boer", email: "info@mikedeboer.nl" },
@@ -1572,7 +1575,6 @@ module.exports = (function(e, t) {
         once: "^1.4.0",
         "universal-user-agent": "^4.0.0"
       },
-      deprecated: false,
       description: "GitHub REST API client for Node.js",
       devDependencies: {
         "@gimenete/type-writer": "^0.1.3",
@@ -3676,6 +3678,7 @@ module.exports = (function(e, t) {
   },
   361: function(e) {
     e.exports = {
+      _args: [["axios@0.19.0", "/Users/matan/dev/waka-box"]],
       _from: "axios@0.19.0",
       _id: "axios@0.19.0",
       _inBundle: false,
@@ -3695,16 +3698,13 @@ module.exports = (function(e, t) {
       },
       _requiredBy: ["/"],
       _resolved: "https://registry.npmjs.org/axios/-/axios-0.19.0.tgz",
-      _shasum: "8e09bff3d9122e133f7b8101c8fbdd00ed3d2ab8",
-      _spec: "axios@0.19.0",
-      _where: "/Users/soramorimoto/src/github.com/matchai/waka-box",
+      _spec: "0.19.0",
+      _where: "/Users/matan/dev/waka-box",
       author: { name: "Matt Zabriskie" },
       browser: { "./lib/adapters/http.js": "./lib/adapters/xhr.js" },
       bugs: { url: "https://github.com/axios/axios/issues" },
-      bundleDependencies: false,
       bundlesize: [{ path: "./dist/axios.min.js", threshold: "5kB" }],
       dependencies: { "follow-redirects": "1.5.10", "is-buffer": "^2.0.2" },
-      deprecated: false,
       description: "Promise based HTTP client for the browser and node.js",
       devDependencies: {
         bundlesize: "^0.17.0",
@@ -4700,6 +4700,14 @@ module.exports = (function(e, t) {
         i = /<meta[\s]+?http-equiv=(['"])content-type\1[\s]+?content=(['"])(.+?)\2/i.exec(
           s
         );
+        if (!i) {
+          i = /<meta[\s]+?content=(['"])(.+?)\1[\s]+?http-equiv=(['"])content-type\3/i.exec(
+            s
+          );
+          if (i) {
+            i.pop();
+          }
+        }
         if (i) {
           i = /charset=(.*)/i.exec(i.pop());
         }
@@ -5393,7 +5401,7 @@ module.exports = (function(e, t) {
               case "error":
                 s(
                   new FetchError(
-                    `redirect mode is set to error: ${u.url}`,
+                    `uri requested responds with a redirect, redirect mode is set to error: ${u.url}`,
                     "no-redirect"
                   )
                 );
@@ -5431,7 +5439,8 @@ module.exports = (function(e, t) {
                   method: u.method,
                   body: u.body,
                   signal: u.signal,
-                  timeout: u.timeout
+                  timeout: u.timeout,
+                  size: u.size
                 };
                 if (
                   e.statusCode !== 303 &&
